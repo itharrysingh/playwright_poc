e6f36be6a9f71830c74afd17434b72cda1a70936
itharrysingh <itharry.singh@gmail.com>
2024-08-22 13:40:48 -0500

    Update authentication.spec.js with negative tests

--- tests/authentication.spec.js
+++ tests/authentication.spec.js
@@ -11,7 +11,7 @@ const testData = [
   { username: 'visual_user', password: 'secret_sauce', expectedSuccess: true },
 ];
 
-test.describe('Authentication tests', () => {
+test.describe('Authentication tests - happy paths', () => {
   testData.forEach(({ username, password, expectedSuccess }) => {
     test(`Login test for ${username}`, async ({ page }) => {
 
@@ -23,9 +23,43 @@ test.describe('Authentication tests', () => {
         await expect(page).toHaveURL('https://www.saucedemo.com/');
       } else {
         await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-
       }
+      await page.close();
+    });
+  })
+})
 
+
+const testData_Negative = [
+  { username: 'standard1_user', password: 'secret_sauce', expectedSuccess: false },
+  { username: 'locked_out__user', password: 'secret_sauce', expectedSuccess: false },
+  { username: 'problem_us%$Her', password: 'secret_sauce', expectedSuccess: false },
+  { username: 'performance___glitch_user', password: 'secret_sauce', expectedSuccess: false },
+  { username: 'erroruser', password: 'secret_sauce', expectedSuccess: false },
+  { username: 'visual_user_', password: 'secret_sauce', expectedSuccess: false },
+  { username: 'standard_user', password: '', expectedSuccess: false },
+  { username: 'locked_out_user', password: 'secret235_sauce', expectedSuccess: false },
+  { username: 'problem_user', password: 'secret_sauce%$%$^dfh', expectedSuccess: false },
+  { username: 'performance_glitch_user', password: 'null', expectedSuccess: false },
+  { username: 'error_user', password: '346656654', expectedSuccess: false },
+  { username: 'visual_user', password: '1', expectedSuccess: false },
+  { username: 'null', password: 'null', expectedSuccess: false },
+  { username: '', password: '', expectedSuccess: false },
+];
+
+test.describe('Authentication tests - negative tests', () => {
+  testData_Negative.forEach(({ username, password, expectedSuccess }) => {
+    test(`Login test for ${username}`, async ({ page }) => {
+
+      await page.goto('/');
+      const loginPage = new LoginPage(page);
+      await loginPage.login(username, password);
+
+      if (expectedSuccess == false) {
+        await expect(page).toHaveURL('https://www.saucedemo.com/');
+      } else {
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+      }
       await page.close();
     });
   })
